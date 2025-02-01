b1fd5747f26f4a56e4d41194d1f38ea44cef8889
londyy123 <londynnista1128@gmail.com>
2025-02-01 11:09:23 -0500

    fixing errors

--- src/main/java/com/codedifferently/labs/partA/ex01/ChkOddEven.java
+++ src/main/java/com/codedifferently/labs/partA/ex01/ChkOddEven.java
@@ -3,21 +3,24 @@ package com.codedifferently.labs.partA.ex01;
 import java.util.Scanner;
 
 public class ChkOddEven {
-    public static String oddEven(){
-        String response = "";
-        Scanner scanner = new Scanner(System.in);
+    public static String oddEven() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter a number between 1 & 10: ");
+            int num = scanner.nextInt();
 
+            if (num >= 1 && num <= 10) {
+                if (num % 2 == 0) {
+                    return "Even number " + num;
+                } else {
+                    return "Odd number " + num;
+                }
+            } else {
+                return "Invalid input. Please enter a number between 1 and 10.";
+            }
+        }
 
-
-
-        return response;
-    }
-    public static void main(String args[]) {
-
-        Scanner in = new Scanner(System.in);
-        int num = 0;
-        System.out.println("Enter a number:");
-        num = in.nextInt();
-
+        {
+            System.out.println(oddEven());
+        }
     }
 }
