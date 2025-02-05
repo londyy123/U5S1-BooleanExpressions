c004332f5b5b1158aa6d8f0078377109ae98bfce
londyy123 <londynnista1128@gmail.com>
2025-02-05 18:30:00 -0500

    Update TempCheck.java

--- src/main/java/com/codedifferently/labs/partB/ex01/TempCheck.java
+++ src/main/java/com/codedifferently/labs/partB/ex01/TempCheck.java
@@ -5,7 +5,27 @@ public class TempCheck {
     public static String bool(){
         String response= "";
 
+        int temp = 66;
 
+        if (temp <= 45) {
+            response += ("It's cold - putting on a coat!");
+        }
+        if (temp >= 46)  {
+            response += ("It's not that bad out - a hoodie will do just fine!");
+        }
+        else {
+            response += ("It's hot - a T shirt will be perfect");
+        }
+
+
+
+            //Create a program that takes in an expected user input as an integer.
+
+                //The program should compare that input and based on the result of the comparison, print out a statement:
+
+        //If input is less than or equal to 45 - "It's cold - putting on a coat!"
+        //If input is greater than or equal to 46 and less than or equal to 65 - "It's not bad out - a hoodie will do just fine!"
+        //Any other situation, - "It's hot - a T shirt will be perfect."
 
 
         return response;
